37449ea2b4ccb8ef8969ec18f0e4e8d573c76603
JonathanMRadai <radai2510@gmail.com>
2025-03-15 17:55:33 +0200

    delete script

--- fp_k8s_deployment/setvar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export AWS_ACCESS_KEY_ID="fake-access-key"
-export AWS_SECRET_ACCESS_KEY="fake-secret-key"
-
-#in order to use it: 
-# source setvar.sh
-# OR using the dot notation
-# . setvar.sh
