677f3b976085c4d04fce40b222069911693c78ec
JonathanMRadai <radai2510@gmail.com>
2025-03-15 13:19:21 +0200

    added script that set env var

---
+++ fp_k8s_deployment/setvar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID="fake-access-key"
+export AWS_SECRET_ACCESS_KEY="fake-secret-key"
+
+#in order to use it: 
+# source setvar.sh
+# OR using the dot notation
+# . setvar.sh
